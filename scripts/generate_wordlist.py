e9784f546e5952eedae5c57d4047125074a5f7a3
Brad Greenlee <brad@footle.org>
2024-12-18 13:33:38 -0800

    Split words into common/uncommon, and only source letters from common

--- scripts/generate_wordlist.py
+++ scripts/generate_wordlist.py
@@ -6,13 +6,23 @@ import re
 rootdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
 datadir = os.path.join(rootdir, "data")
 
-# read the NASPA word list
+# read the main word list
 with open(os.path.join(datadir, "wordnik.txt")) as f:
     nwl_words = f.read().strip().split("\n")
+
+# read the top N words list
+with open(os.path.join(datadir, "top-words.txt")) as f:
+    top_words = set(f.read().strip().split("\n"))
 
 # filter out invalid words
 nwl_words = set([w for w in nwl_words if len(w) >= 4])
 
+common_words = nwl_words.intersection(top_words)
+uncommon_words = nwl_words - common_words
+
 # output the final word list as a single space-separated string
-with open(os.path.join(datadir, "words-filtered"), "w") as f:
-    f.write(" ".join(sorted(nwl_words)))
+with open(os.path.join(datadir, "words-common"), "w") as f:
+    f.write(" ".join(sorted(common_words)))
+
+with open(os.path.join(datadir, "words-uncommon"), "w") as f:
+    f.write(" ".join(sorted(uncommon_words)))
