0fa8ef3e63041a929b00a004f16748be1bc0eab7
Brad Greenlee <brad@footle.org>
2024-12-19 14:05:50 -0800

    Use nwl

--- scripts/generate_wordlist.py
+++ scripts/generate_wordlist.py
@@ -5,9 +5,10 @@ import re
 # this script is meant to be run from the root directory of the project
 rootdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
 datadir = os.path.join(rootdir, "data")
+jsdir = os.path.join(rootdir, "js")
 
 # read the main word list
-with open(os.path.join(datadir, "wordnik.txt")) as f:
+with open(os.path.join(datadir, "nwl2020.txt")) as f:
     nwl_words = f.read().strip().split("\n")
 
 # read the top N words list
@@ -20,9 +21,11 @@ nwl_words = set([w for w in nwl_words if len(w) >= 4])
 common_words = nwl_words.intersection(top_words)
 uncommon_words = nwl_words - common_words
 
-# output the final word list as a single space-separated string
-with open(os.path.join(datadir, "words-common"), "w") as f:
+# output the final word list to words.js
+with open(os.path.join(jsdir, "words.js"), "w") as f:
+    f.write("export const Words = {\n  common: `")
     f.write(" ".join(sorted(common_words)))
-
-with open(os.path.join(datadir, "words-uncommon"), "w") as f:
+    f.write("`,\n  uncommon: `")
     f.write(" ".join(sorted(uncommon_words)))
+    f.write("`\n};")
+
